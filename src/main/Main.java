6a93d8d00c7a8ed211b2c731229102d376a80081
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-07-03 16:24:33 -0300

    Adicionando entrada do usuário.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.io.IOError;
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 import business.algoritmos.AEstrela;
 import business.control.GerentePontosDeOnibus;
@@ -17,16 +21,25 @@ public class Main{
 		List<PontoDeOnibus> pontos = gerentePontos.getPontosDeOnibus();
 		MatrizDeDistancias distancias = gerentePontos.getMatrizDeDistancias();
 		ListaDeAdjacencia vizinhos = gerentePontos.getListaDeAdjacencia();*/
-		
-		int idOrigem = 34 - 1;
-		int idDestino = 5 - 1;
-		
-		PontoDeOnibus origem = GerentePontosDeOnibus.getInstancia().getPontoDeOnibus(idOrigem);
-		PontoDeOnibus destino = GerentePontosDeOnibus.getInstancia().getPontoDeOnibus(idDestino);
-		AEstrela.getInstancia().buscaAEstrela(origem, destino);
-		
-		System.out.println("Programa encerrado");
 
+		try{
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Informe a origem: ");
+			int idOrigem = scan.nextInt() - 1;
+
+			System.out.println("Informe o destino: ");
+			int idDestino = scan.nextInt() - 1;
+
+			if(idOrigem < 0 || idOrigem > 35 || idDestino < 0 || idDestino > 35)
+				throw new InputMismatchException();
+
+			PontoDeOnibus origem = GerentePontosDeOnibus.getInstancia().getPontoDeOnibus(idOrigem);
+			PontoDeOnibus destino = GerentePontosDeOnibus.getInstancia().getPontoDeOnibus(idDestino);
+			AEstrela.getInstancia().buscaAEstrela(origem, destino);
+
+		}catch(InputMismatchException e){
+			System.out.println("Erro: digite valores entre 1 e 35!");
+		}
 	}
 
 }
